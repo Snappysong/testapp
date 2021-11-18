884c2b73f3c7031a2d395df41885e9281a71c749
Andrew Song <snappysong@gmail.com>
2021-11-17 17:34:49 -0800

    initial upload

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learning MORE React
         </a>
       </header>
     </div>
